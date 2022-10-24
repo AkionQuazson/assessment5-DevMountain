189b483492c5fbe1c7a98f693a1925f2e524f33a
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-10-24 17:44:36 -0600

    compleated

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -228,5 +228,49 @@ module.exports = {
             console.log('DB seeded!')
             res.sendStatus(200)
         }).catch(err => console.log('error seeding DB', err))
+    },
+    getCountries: (req, res) => {
+        sequelize.query(`
+            select * from countries;
+        `).then(
+            (dbRes) => {
+                res.status(200).send(dbRes[0])
+             }
+        ).catch((err) => console.log(err))
+    },
+	createCity: (req, res) => {
+		const {name, rating, countryId} = req.body;
+		sequelize.query(`
+			insert into cities (name, rating, country_id)
+			values ('${name}', ${rating}, ${countryId});
+		`).then(
+			(dbRes) => {
+				res.status(200).send(dbRes[0])
+			 }
+		).catch((err) => console.log(err))
+	},
+    getCities: (req, res) => {
+		sequelize.query(`
+        select city_id, cities.name as city, rating, cities.country_id, countries.name as country
+            from cities
+            join countries
+                on cities.country_id = countries.country_id
+				order by rating desc;
+        `).then(
+            (dbRes) => {
+                res.status(200).send(dbRes[0])
+             }
+        ).catch((err) => console.log(err))
+    },
+    deleteCity: (req, res) => {
+        const {id} = req.params
+		sequelize.query(`
+			delete from cities 
+			where city_id = ${id};
+        `).then(
+			(dbRes) => {
+				res.status(200).send(dbRes[0])
+			 }
+		).catch((err) => console.log(err))
     }
 }
